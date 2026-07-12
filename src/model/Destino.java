e7587717cc36806bedb5dd616cafe300420a1caa
saulfabre <saulfabre2@gmail.com>
2026-07-11 23:26:05 -0400

    Implementar clase Destino

--- src/model/Destino.java
+++ src/model/Destino.java
@@ -1,5 +1,19 @@
 package model;
 
 public class Destino {
+
+    private String nombre;
+
+    public Destino(String nombre) {
+        setNombre(nombre);
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
 }
