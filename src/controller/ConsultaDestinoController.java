81bf2d0cee9470be8be2a31a1cfae7bca3b5e25a
saulfabre <saulfabre2@gmail.com>
2026-07-12 10:53:55 -0400

    Implementar carga de destinos en el ListView

--- src/controller/ConsultaDestinoController.java
+++ src/controller/ConsultaDestinoController.java
@@ -1,5 +1,36 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import model.Destino;
+import utils.ArchivoUtil;
+
 public class ConsultaDestinoController {
-    
+
+    @FXML
+    private Label lblEstado = new Label();
+
+    @FXML
+    private ListView<Destino> listViewDestinos = new ListView<>();
+
+    @FXML
+    private void cargarDestinos() {
+
+        ObservableList<Destino> destinos = FXCollections.observableArrayList(
+
+            ArchivoUtil.leerDestinos());
+
+        listViewDestinos.setItems(destinos);   
+
+        if (destinos.isEmpty()) {
+
+            lblEstado.setText("No hay destinos registrados.");
+            return;
+        }
+        
+        lblEstado.setText("Destinos cargados exitosamente.");
+    }   
 }
